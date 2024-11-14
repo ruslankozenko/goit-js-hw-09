a2a7d95ded6e1b49665a00a7fd9297b7cc33fc28
Ruslan Kozenko <kozenko.r@seznam.cz>
2024-11-14 16:48:53 +0100

    hw9 form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,2 +1,62 @@
 'use strict';
 
+const formData = {
+    email: "",
+    message: "",
+};
+
+
+function saveToLocalStorage() {
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+}
+
+
+function fillFormFromLocalStorage() {
+    const savedData = localStorage.getItem("feedback-form-state");
+    if (savedData) {
+        const parsedData = JSON.parse(savedData);
+        formData.email = parsedData.email || "";
+        formData.message = parsedData.message || "";
+        document.querySelector('[name="email"]').value = formData.email;
+        document.querySelector('[name="message"]').value = formData.message;
+    }
+}
+
+
+document.querySelector(".feedback-form").addEventListener("input", (event) => {
+    const { name, value } = event.target;
+    // Оновлюємо значення у formData
+    if (name === "email") {
+        formData.email = value.trim();
+    } else if (name === "message") {
+        formData.message = value.trim();
+    }
+
+    saveToLocalStorage();
+});
+
+
+document.querySelector(".feedback-form").addEventListener("submit", (event) => {
+    event.preventDefault();
+
+
+    if (!formData.email || !formData.message) {
+        alert("Fill please all fields");
+        return;
+    }
+
+
+    console.log(formData);
+
+
+    localStorage.removeItem("feedback-form-state");
+    formData.email = "";
+    formData.message = "";
+
+
+    document.querySelector('[name="email"]').value = "";
+    document.querySelector('[name="message"]').value = "";
+});
+
+
+fillFormFromLocalStorage();

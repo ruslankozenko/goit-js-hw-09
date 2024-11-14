6c15314f8764764d5bd3d3e893c6ede2e232ef08
ruslankozenko <147592463+ruslankozenko@users.noreply.github.com>
2024-11-14 14:00:23 +0000

    Deploying to gh-pages from @ ruslankozenko/goit-js-hw-09@4ee48d9a16acea539aeb1755d98837a578dae801 🚀

--- 1-gallery.js
+++
@@ -1,2 +0,0 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{S as t}from"./assets/vendor-CgTBfC_f.js";document.querySelector(".gallery");new t(".gallery-link",{captionsData:"alt",captionDelay:250});
-//# sourceMappingURL=1-gallery.js.map
